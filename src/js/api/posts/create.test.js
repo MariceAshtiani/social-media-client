0a6a637e6fcbfba1cdd5ef23fe8b64264d02e069
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-01-25 21:20:11 +0100

    create test error

---
+++ src/js/api/posts/create.test.js
@@ -0,0 +1,26 @@
+import { createPost } from "./create.mjs";
+
+const mockObject = {
+    title: "title",
+    body: "body",
+    tags: ["these", "are", "tags"],
+    media: "https://images.pexels.com/photos/11459355/pexels-photo-11459355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    id: 2324
+};
+
+function createPostMockSuccess() {
+    return Promise.resolve({
+        status: 200,
+        ok: true,
+        statusText: "Approved",
+        json: () => Promise.resolve(mockObject),
+    });
+}
+
+describe("create", () => {
+    it("returns a valid item with a valid input", async () => {
+        global.fetch = jest.fn(() => createPostMockSuccess());
+        const result = await createPost("title, body, tags, media");
+        expect(post).toMatchObject(mockObject);
+    });
+});
