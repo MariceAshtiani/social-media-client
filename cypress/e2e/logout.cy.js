e7e097d4a03f4a442cdb1c8a07ae21892910ac05
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-01-27 13:21:46 +0100

    Log out test working

---
+++ cypress/e2e/logout.cy.js
@@ -0,0 +1,34 @@
+// The user can log out with the log out button
+
+const EMAIL = "thisismyemail@stud.noroff.no";
+const PASSWORD = "thisismypassword";
+
+describe("logout", () => {
+    beforeEach(() => {
+        cy.clearLocalStorage();
+        cy.visit("./");
+    });
+
+    it("logs out user with logout button", () => {
+        cy.get("#registerModal button")
+            .contains("Login")
+            .should("be.visible")
+            .wait(1000)
+            .click()
+            .wait(500);
+        cy.get("input#loginEmail[name='email']").type(EMAIL);
+        cy.get("input#loginPassword[name='password']").type(PASSWORD);
+        cy.wait(1000);
+        cy.get("button[type='submit']").contains("Login").click();
+        cy.wait(1000);
+        cy.then(() => expect(localStorage.getItem("token")).to.not.be.null);
+        cy.wait(2000);
+
+        cy.get("button[data-auth='logout']")
+            .click()
+            .should(() => {
+                expect(localStorage.getItem('token')).to.be.null
+            });
+        cy.location("pathname").should("eq", "/");
+    })
+})
