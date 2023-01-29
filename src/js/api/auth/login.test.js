2a1a025c3e555b2423946fbb105dcbeb3aad82a1
MariceAshtiani <95356770+MariceAshtiani@users.noreply.github.com>
2023-01-29 14:46:12 +0100

    minor changes on login unit test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,20 +1,5 @@
 import { login } from "./login";
 
-// create a mock fetch function that successfully fetches profile info
-
-const mockFetchSuccess = jest.fn().mockResolvedValue({
-    ok: true,
-    json: jest.fn().mockResolvedValue({
-        name: "thisismyname",
-        email: "thisisanemail@stud.noroff.no",
-        banner: null,
-        avatar: null,
-        accessToken: "thisismyname's token",
-    }),
-});
-
-global.fetch = mockFetchSuccess;
-
 // creates a fake localStorage (there is no localStorage in nodeJS' environment where jest runs)
 
 class LocalStorageMock {
@@ -34,9 +19,26 @@ class LocalStorageMock {
         this.store[key] = String(value);
     }
 }
+
 // declare the mock localStorage so it can be used for mutliple tests if needed.
 
 global.localStorage = new LocalStorageMock;
+
+// create a mock fetch function that successfully fetches profile info
+
+const mockFetchSuccess = jest.fn().mockResolvedValue({
+    ok: true,
+    json: jest.fn().mockResolvedValue({
+        name: "thisismyname",
+        email: "thisisanemail@stud.noroff.no",
+        banner: null,
+        avatar: null,
+        accessToken: "thisismyname's token",
+    }),
+});
+
+global.fetch = mockFetchSuccess;
+
 
 // the unit test to check if the login function works, and stores a token when provided with valid credentials
 // if it works, we would expect the token not to be null = the token is being stored.
